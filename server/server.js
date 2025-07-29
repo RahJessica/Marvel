09f43c3c383c812cb10772125348755b4b612ca9
RahJess <jessicakantotiana@gmail.com>
2025-07-29 21:53:57 +0200

    feat: add routes and endpoints to search characters by id or display all characters

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,3 +8,31 @@ app.use(cors());
 app.use(express.json());
 
 const marvels = require('./user.json');
+const DATA_FILE = './user.json';
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}`);
+});
+
+function readData() {
+  const rawData = fs.readFileSync(DATA_FILE);
+  return JSON.parse(rawData).characters;
+}
+function writeData(characters) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify({ characters }, null, 2));
+}
+
+app.get('/characters', (req, res) => {
+  const characters = readData();
+  res.json(characters);
+});
+
+app.get('/characters/:id', (req, res) => {
+  const characters = readData();
+  const character = characters.find(c => c.id === parseInt(req.params.id));
+  if (character) {
+    res.json(character);
+  } else {
+    res.status(404).json({ error: 'Character not found' });
+  }
+});
