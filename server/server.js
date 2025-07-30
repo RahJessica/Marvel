66d8e96894b49e0d0f7fe62646fd9f6358d0c23f
RahJess <jessicakantotiana@gmail.com>
2025-07-30 19:55:33 +0200

    add: route to delete characters by ID

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require('cors');
 const PORT = 3001;
 
+// lancer le server express : node server.js
+
 app.use(cors());
 app.use(express.json());
 
@@ -48,5 +50,20 @@ app.put('/characters/:id', (req, res) => {
     data.characters[index] = { id, ...updatedCharacter };
     writeData(data);
     res.json(data.characters[index]);
+  }
+});
+
+app.delete('/characters/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const data = readData();
+  const index = data.characters.findIndex(char => char.id === id);
+
+  if (index !== -1) {
+    const deleted = data.characters.splice(index, 1);
+    writeData(data);
+    res.json({ message: 'Character deleted', character: deleted[0] });
+  } else {
+    res.status(404).json({ error: 'Character not found' });
   }
 });
